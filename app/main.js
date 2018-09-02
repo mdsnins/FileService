0c5be923c555a49777ed6527608b9e8ba5bec3ec
juchang <c2w2@naver.com>
2018-09-02 16:46:37 +0900

    Add: Extension - main.js

--- app/main.js
+++ app/main.js
@@ -1,25 +1,42 @@
 module.exports = function (app, fs, path) {
-    function getFileList(path_) {
-        let result = new Array()
-        let items = fs.readdirSync(path_)
-
-        items.forEach(element => {
-            if(element[0] != '.')
-                result.push({ name: element, directory: fs.existsSync(path_ + '/' + element + '/') ? 1 : 0 })
-        })
-        
-        result.sort((a, b) => {
-           return b.directory - a.directory
-        })
-        return result
-    }
-
     function getExtension(name){
         if(name.search('.') != 0)
             return ''
         
         return name.substring(name.indexOf('.') + 1, name.length)
     }
+
+    function getType(extension){
+        let img = ['png', 'jpg', 'gif']
+        let txt = ['md', 'txt']
+        let zip = ['zip', '7z', 'tar', 'gz']
+
+        if(img.includes(extension))
+            return 'image'
+        else if(txt.includes(extension))
+            return 'txt'
+        else if(zip.includes(extension))
+            return 'zip'
+        else
+            return 'file'
+    }
+    
+    function getFileList(path_) {
+        let result = new Array()
+        let items = fs.readdirSync(path_)
+
+        items.forEach(element => {
+            if(element[0] != '.' && element != "js" && element != "css" && element != "img")
+                result.push({ name: element, type: fs.existsSync(path_ + '/' + element + '/') ? 'folder' : getType(getExtension(element))})
+        })
+        
+        result.sort((a, b) => {
+            if (a.type == "folder") return 1
+        })
+
+        return result.reverse()
+    }
+
 
     app.get('/*', (req, res) => {
         let param = req.param(0)
@@ -33,9 +50,9 @@ module.exports = function (app, fs, path) {
         } else {
             if(fs.existsSync(folderpath)){
                 let extension = getExtension(folderpath)
-                let noeDownload = ['txt', 'md', 'css', 'html', 'js', 'png', 'jpg', 'gif']
+                let noDownload = ['txt', 'md', 'css', 'html', 'js', 'png', 'jpg', 'gif']
                 if(extension != ''){
-                    if(noeDownload.includes(extension))
+                    if(noDownload.includes(extension))
                         res.sendFile(folderpath)
                     else
                         res.download(folderpath)
@@ -46,3 +63,4 @@ module.exports = function (app, fs, path) {
         }
     })
 }
+
