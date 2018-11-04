ecbfb7ea55c3f7d07cd9e46ed9087978fcdc68b4
juchang <c2w2@naver.com>
2018-11-04 20:37:56 +0900

    Add: util.js

--- app/main.js
+++ app/main.js
@@ -1,54 +1,6 @@
-module.exports = function (app, fs, path) {
-    function getExtension(name){
-        if(name.search('.') != 0)
-            return ''
-        
-        return name.substring(name.indexOf('.') + 1, name.length)
-    }
+module.exports = (app, fs, path) => {
 
-    function getType(extension){
-        let file_sig = {
-            'img': [
-                'png',
-                'jpg',
-                'gif',
-		'ico'
-            ],
-            'txt': [
-                'md',
-                'txt'
-            ],
-            'zip': [
-                'zip',
-                '7z',
-                'tar',
-                'gz'
-            ],
-'ppt':['pptx','ppt']
-        }
-        for(let i in file_sig){
-            if(file_sig[i].includes(extension))
-                return i
-        }
-        return 'file'
-    }
-    
-    function getFileList(path_) {
-        let result = new Array()
-        let items = fs.readdirSync(path_)
-
-        items.forEach(element => {
-            if(element[0] != '.' && element != "js" && element != "css" && element != "img")
-                result.push({ name: element, type: fs.existsSync(path_ + '/' + element + '/') ? 'folder' : getType(getExtension(element))})
-        })
-        
-        result.sort((a, b) => {
-            if (a.type == "folder") return 1
-        })
-
-        return result.reverse()
-    }
-
+    require('./util')
 
     app.get('/*', (req, res) => {
         let param = req.param(0)
---
+++ app/util.js
@@ -0,0 +1,54 @@
+module.exports = () => {
+    function getExtension(name){
+        if(name.search('.') != 0)
+            return ''
+        
+        return name.substring(name.indexOf('.') + 1, name.length)
+    }
+    
+    function getType(extension){
+        let file_sig = {
+            'img': [
+                'png',
+                'jpg',
+                'gif',
+                'ico'
+            ],
+            'txt': [
+                'md',
+                'txt'
+            ],
+            'zip': [
+                'zip',
+                '7z',
+                'tar',
+                'gz'
+            ],
+            'ppt':[
+                'pptx',
+                'ppt'
+            ]
+        }
+        for(let i in file_sig){
+            if(file_sig[i].includes(extension))
+                return i
+        }
+        return 'file'
+    }
+    
+    function getFileList(path_) {
+        let result = new Array()
+        let items = fs.readdirSync(path_)
+    
+        items.forEach(element => {
+            if(element[0] != '.' && element != "js" && element != "css" && element != "img")
+                result.push({ name: element, type: fs.existsSync(path_ + '/' + element + '/') ? 'folder' : getType(getExtension(element))})
+        })
+        
+        result.sort((a, b) => {
+            if (a.type == "folder") return 1
+        })
+    
+        return result.reverse()
+    }
+}
